975112858647a90a9c82723ae69276017d1840f4
Renato <renatoxavier.rx@hotmail.com>
2022-05-14 14:05:25 -0300

    creating the routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 import express from "express";
-
-const test = 1;
-
-const app = express();
+import { routes } from './routes';
 
 //GET , POST, PUT , PATCH, DELETE
 //GET - Buscar informações
@@ -11,21 +9,9 @@ const app = express();
 //PUT = Atualizar informações de uma entidade
 //Patch = atualizar um informação única de uma entidade
 //DELETE = deletar informações
+const app = express();
 app.use(express.json());
-
-app.post('/feedbacks', async (req, res) => {
-    const { type, comment, screenshot } = req.body
-    
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        }
-    })
-
-    return res.status(201).json({data: feedback})
-});
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('HTTP server running')
